987ae5800f17bbc94f6a836cba1427910e0a99a6
Baktai-Norbert <norbi.baktai2005@gmail.com>
2021-05-02 10:59:29 +0200

    0.12
    +1. html oldal 1. aloldala
    +JavaScript elkezdése
    +alapok

---
+++ projekt5 Metro Baktai Norbert/0.12 projekt5 Metro Baktai Norbert/JavaScript/Scipt.js
@@ -0,0 +1,46 @@
+function metro33() {
+   console.log("műkszik");
+   let cim1 = document.querySelector('h1');
+   cim1.innerText = "Metro 2033";
+   
+   let tört = document.getElementById('tort');
+   tört.innerHTML = "A történet 2033-ban játszódik, a moszkvai metróhálozatban. A játékban 2013-ban kitört a III. világháború, és megtörtént a nukleáris apokalipszis. Te 20 évvel később, egy Artyom nevű túlélőként játszol. Minden nap egy megpróbáltatás az embereknek a földalatt. Egyszercsak felbukkantak a Dark One nevű szörnyek, akik nagy fenyegetést jelentenek az állomásodra. Artyom példaképe Hunter pedig elküldi Artyomot a metró központjába, hogy értesítse feletteseit a szörnyekről, és kérjen segítséget.";
+
+   let menet = document.getElementById('menet');
+   menet.innerHTML = "Játék alapvetően belső nézetes lövölde, azonban a lopakodási mechanikák sokat hozzá tesznek a játékhoz. A   környezet dinamikus, ezért kihasználhatod ellenségeid ellen. A játékot lehet pacifistán is végigjátszani. Érdekesség, hogy a játékban nem pénz van, hisz az értéktelenné vált a járatokban. Helyette lőszerrel lehet kereskedni. A játékban figyelni kell, hogy fel legyen töltve a zseblámpád, ne legyen a gázmaszkod törött, és legyen bele elég szűrő.";
+
+   let karakter = document.getElementById('karakter');
+   karakter.innerHTML = "A játékban sok emberrel találkozol, viszont ők a fontosabbak:";
+
+   let karakterul  = document.getElementById('karakterul');
+   karakterul.innerHTML = "<li>Hunter: ő a példaképed. Sajnálatos módon a játék elején láthatod egyedül, mikoris elküld téged Polisba, a metró központjába.</li> <li>Bourbon: vele a játék elején ismerkedsz meg. Nem a legkedvesebb személy, de segített neked utadon, mert te is segítettél neki.</li> <li> Khan: Nagyon rejtélyes alak. Hisz abban, hogy a Metrónak lelke van. Segít neked utadon, és próbál minnél jobban a jó útra terelni.</li> <li> Ulner: Egy Ranger, a Rend tagja és egy komikus. Próbálja humorral elrejteni valós fájdalmait és érzéseit.</li> <li> Miller: Ő a Rend parancsnoka, és ő igazgat Polisban. Később is segít neked a játékban.</li>";
+
+   let bek1 = document.getElementById('bek1');
+   bek1.innerHTML = "<h2>Vizuális élmény</h2> <p>A játék grafikája szerintem a mai napig szépnek számít, annak ellenére, hogy 2010-es, illetve a felújított verziója 2014-es.</p> <p>A fény effektek véleményem szerint gyönyörűek. Nemcsak gyönyörűek, hanem hasznosak is. A fényforrásokat ki lehet kapcsolni vagy lőni, hogy nehezebben lássanak meg ellenségeid.</p> <p>A textúrák, de főleg a normálok remek minőségüek. A modellek kidolgozottak, azonban néhány animáció kicsit alacsonyabb minőségű.</p> <p>Nekem a játékban a FOV (Field Of View = Látószög) kicsit alacsony, cserébe a játék jól optimalizált.</p> <p>A játék nagyon atmoszférikus, és a pályák dizájnai is kiemelkedők.</p>";
+}
+
+function ll() {
+   console.log("műkszik2");
+   let cim2 = document.querySelector('h1');
+   cim2.innerText = "Metro Last Light";
+   
+   let tört = document.getElementById('tort');
+   tört.innerHTML = "A történet 1 évvel később játszódik, 2034-ben. Khan beront a barakkodba, mondva hogy szerencsére mégegy dark one túlélte. A parancsnok, Miller lányával elküld téged levadászni a szörnyet. Mikor a szörny karnyújtásnyira van tőled elkábít, és arra ébredsz, hogy fasiszták elfognak. Egy kommunista fogoly segítségével azonban onnan kimenekültök, és azt mondja segít neked eljutni a központba. Vajon meg bízhatsz benne? Mi lesz a lénnyel?";
+
+   let menet = document.getElementById('menet');
+   menet.innerHTML = "Játék alapvetően belső nézetes lövölde, azonban a lopakodási mechanikák sokat hozzá tesznek a játékhoz. A   környezet dinamikus, ezért kihasználhatod ellenségeid ellen. A játékot lehet pacifistán is végigjátszani. Érdekesség, hogy a játékban nem pénz van, hisz az értéktelenné vált a járatokban. Helyette lőszerrel lehet kereskedni. A játékban figyelni kell, hogy fel legyen töltve a zseblámpád, ne legyen a gázmaszkod törött, és legyen bele elég szűrő.";
+
+   let karakter = document.getElementById('karakter');
+   karakter.innerHTML = "A játékban sok emberrel találkozol, viszont ők a fontosabbak:";
+
+   let karakterul  = document.getElementById('karakterul');
+   karakterul.innerHTML = "<li>Hunter: ő a példaképed. Sajnálatos módon a játék elején láthatod egyedül, mikoris elküld téged Polisba, a metró központjába.</li> <li>Bourbon: vele a játék elején ismerkedsz meg. Nem a legkedvesebb személy, de segített neked utadon, mert te is segítettél neki.</li> <li> Khan: Nagyon rejtélyes alak. Hisz abban, hogy a Metrónak lelke van. Segít neked utadon, és próbál minnél jobban a jó útra terelni.</li> <li> Ulner: Egy Ranger, a Rend tagja és egy komikus. Próbálja humorral elrejteni valós fájdalmait és érzéseit.</li> <li> Miller: Ő a Rend parancsnoka, és ő igazgat Polisban. Később is segít neked a játékban.</li>";
+
+   let bek1 = document.getElementById('bek1');
+   bek1.innerHTML = "<h2>Vizuális élmény</h2> <p>A játék grafikája szerintem a mai napig szépnek számít, annak ellenére, hogy 2010-es, illetve a felújított verziója 2014-es.</p> <p>A fény effektek véleményem szerint gyönyörűek. Nemcsak gyönyörűek, hanem hasznosak is. A fényforrásokat ki lehet kapcsolni vagy lőni, hogy nehezebben lássanak meg ellenségeid.</p> <p>A textúrák, de főleg a normálok remek minőségüek. A modellek kidolgozottak, azonban néhány animáció kicsit alacsonyabb minőségű.</p> <p>Nekem a játékban a FOV (Field Of View = Látószög) kicsit alacsony, cserébe a játék jól optimalizált.</p> <p>A játék nagyon atmoszférikus, és a pályák dizájnai is kiemelkedők.</p>";
+}
+function me() {
+   console.log("műkszik3");
+   let cim3 = document.querySelector('h1');
+   cim3.innerText = "Metro Exodus";
+}
